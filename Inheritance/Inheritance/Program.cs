eb23a752038b5ad6df393562d5637783430974e2
TanvirRahman <tanvir.rahman.ewu@gmail.com>
2020-06-14 13:02:40 +0600

    inheritance and polymorphism added

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Car //base class
+    {
+        private string name;
+
+        public void set_name(string name)
+        {
+            this.name = name;
+        }
+
+        public string get_name()
+        {
+            return this.name;
+        }
+    }
+
+    class Bus : Car
+    {
+        public void display()
+        {
+            Console.WriteLine(this.get_name()); //calling base class method to access private field of base class
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bus b = new Bus();
+            b.set_name("hudai");
+            b.display();
+        }
+    }
+}
---
+++ polymorphism/polymorphism/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace polymorphism
+{
+    //we cant create instance of abstract class rather we can use it as reference
+    abstract class Animal //its quite like interface
+    {
+        public abstract void sound();
+    }
+
+    class Cow : Animal
+    {
+        public override void sound()
+        {
+            Console.WriteLine("Hambaaaa");
+        }
+    }
+
+    class Goat : Animal
+    {
+        public override void sound()
+        {
+            Console.WriteLine("mee mee");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cow = new Cow();
+            Animal goat = new Goat();
+            
+
+            cow.sound();
+            goat.sound();
+        }
+    }
+}
