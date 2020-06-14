b23efe22e935ac9d8f0561a5106a52f288004204
TanvirRahman <tanvir.rahman.ewu@gmail.com>
2020-06-14 12:14:45 +0600

    encapsulation implemented

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    public class Car
+    {
+        private string name;
+
+        public void set_name(string name)
+        {
+            this.name = name;
+        }
+
+        public string get_name()
+        {
+            return this.name;
+        }
+    }
+    class Program
+    {
+        // to keep sensetive data make field as private and access them through public method of same class
+        // this is called encapsulation
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Car c = new Car();
+            c.set_name("Ferrari");
+
+            string name = c.get_name();
+
+            Console.WriteLine(name);
+        }
+    }
+}
