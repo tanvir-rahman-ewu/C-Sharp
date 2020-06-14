fef971e65b4a52072b156165147b106ea4f66272
TanvirRahman <tanvir.rahman.ewu@gmail.com>
2020-06-14 11:35:01 +0600

    class and obj

---
+++ Claas_and_obj/Claas_and_obj/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Claas_and_obj
+{
+    class Television
+    {
+        private string name { get; set; }
+        private int price { get; set; }
+
+        public Television(string name, int price) //constructor
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public void display()
+        {
+            Console.WriteLine(this.price);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Television tv = new Television("sda",10);
+            tv.display();
+
+            
+        }
+    }
+}
