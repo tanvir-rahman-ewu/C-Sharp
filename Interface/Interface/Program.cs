54cef8b035876e5a31471ef2b0f953486a78af30
TanvirRahman <tanvir.rahman.ewu@gmail.com>
2020-06-16 12:11:36 +0600

    Interface

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interface
+{
+    public interface IAnimal
+    {
+        void voice();
+    }
+
+    public class Animal : IAnimal
+    {
+        public Animal( string val)
+        {
+            name = val;
+        }
+        public string name { get; set; }
+        public void voice()
+        {
+            Console.WriteLine("I am Animal");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             IAnimal a = new Animal("Hati");
+
+             a.voice();
+
+            Animal cat = new Animal("Hamba");
+
+
+
+            List<Animal> l = new List<Animal>
+            {
+                new Animal("goru"),
+                new Animal("hash"),
+                new Animal("murgi")
+            };
+
+            foreach( Animal b in l)
+            {
+                b.voice();
+                Console.WriteLine(b.name);
+            }
+        }
+    }
+}
